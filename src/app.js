33954c67c3222687808d14b14e9df9994143cd7e
Joe Dukes <joedukes.email@gmail.com>
2019-06-20 10:55:56 -0400

    005-pet-props

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Nick", animal: "Dog", breed: "Lab Mix" }),
+    React.createElement(Pet, { name: "Tucker", animal: "Dog", breed: "Mutt" }),
+    React.createElement(Pet, {
+      name: "Fred",
+      animal: "Frog",
+      breed: "Bull Frog"
+    }),
+    React.createElement(Pet, {
+      name: "Wayne",
+      animal: "Llama",
+      breed: "Llama Mix"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
